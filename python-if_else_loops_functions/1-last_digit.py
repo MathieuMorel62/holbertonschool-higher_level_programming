421d8def116d06d0767d15a290c65d6746fe0302
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-16 21:06:08 +0100

    delete space around the equals

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number > 0:
 else:
     last_digit = number % -10
 
-print("Last digit of " + str(number) + " is " + str(last_digit), end = " ")
+print("Last digit of " + str(number) + " is " + str(last_digit), end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
