12ceda77c0f192b606626b66621bcfb7e5d68403
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-17 14:24:13 +0100

    add space after the last print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for numberOne in range(10):
     for numberTwo in range(numberOne+1, 10):
         if (numberOne == 8 and numberTwo == 9):
-            print("{}{}".format(numberOne, numberTwo), end="")
+            print("{}{}\n".format(numberOne, numberTwo), end="")
         else:
             print("{}{}".format(numberOne, numberTwo), end=", ")
