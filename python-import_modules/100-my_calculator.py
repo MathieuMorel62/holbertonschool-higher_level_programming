fed53694caa67d84d0279666b03a54412f6a51f5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-19 10:43:03 +0100

    program that imports all functions from the file calculator_1.py and handles basic operations.

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from calculator_1 import *
+import sys
+
+if __name__ == '__main__':
+    numberArg = sys.argv
+    if len(numberArg) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    
+    a = int(numberArg[1])
+    operator = numberArg[2]
+    b = int(numberArg[3])
+    if operator not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
