c5116e33e14ab047242dcb65355c3a73b95b38a0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-23 14:10:08 +0100

    fix bug for the negative number

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,7 +6,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    for i in my_list:
+    max_num = my_list[0]
+    for i in my_list[1:]:
         if i > max_num:
             max_num = i
     return(max_num)
