2972e4a6fcda174d0ff29ad0b8cf386c9f989c06
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-23 11:31:44 +0100

    indentation loop inside of condition

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+
     if my_list:
         my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+        for i in my_list:
+            print("{:d}".format(i))
