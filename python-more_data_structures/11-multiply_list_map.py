766effd5d91c90fa12a81e9d5915363570522b80
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-23 20:20:56 +0100

    delete line break

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-
 def multiply_list_map(my_list=[], number=0):
-    mapped_number = list(map(lambda x: x * number, my_list))
-    return mapped_number
+    return list(map(lambda x: x * number, my_list))
