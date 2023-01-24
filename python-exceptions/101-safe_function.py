415e97fb01d50ad7c17a421cb7e6706a3d678b8d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-24 19:50:42 +0100

    function that executes a function safely.

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+import sys
 
 
 def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as message:
+        print("Exception: {}".format(message), file=sys.stderr)
+        return None
