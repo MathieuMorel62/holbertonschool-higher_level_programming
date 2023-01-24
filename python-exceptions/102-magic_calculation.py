a6374c7f05358409b4aeb7732495d6e4176ea169
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-24 20:22:49 +0100

    Python function def magic_calculation(a, b)

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,3 +2,14 @@
 
 
 def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result = result + (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
