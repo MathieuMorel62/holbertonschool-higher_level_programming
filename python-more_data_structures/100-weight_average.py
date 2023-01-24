31e72899b9214b7b0c3abf1e3901299e1a3781ad
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-24 13:05:43 +0100

    Fix division by zero error when input list is empty or all elements have a weight of 0

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -6,7 +6,7 @@ def weight_average(my_list=[]):
     sum_of_products = 0
     sum_of_weights = 0
 
-    if my_list is None:
+    if not my_list:
         return 0
     for score, weight in my_list:
         sum_of_products += score * weight
