d81fe3ef1b2fc591696e3614b3639db1c2581e02
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-30 18:38:48 +0100

    adding description

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -2,16 +2,31 @@
 
 
 class Node:
+    """ Defines a node in a singly linked list """
     def __init__(self, data, next_node=None):
+        """
+        Initialize the node with data and next_node
+        :param data: Integer data for the node
+        :param next_node: Next node in the list, defaults to None
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        Getter for the data attribute
+        :return: Integer data for the node
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Setter for the data attribute
+        :param value: Integer data for the node
+        :raises TypeError: if data is not an integer
+        """
         if type(value) is not int:
             raise TypeError("data must be an integer")
         else:
@@ -19,10 +34,19 @@ class Node:
 
     @property
     def next_node(self):
+        """
+        Getter for the next_node attribute
+        :return: Next node in the list
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Setter for the next_node attribute
+        :param value: Next node in the list
+        :raises TypeError: if next_node is not a Node object or None
+        """
         if value is not None and type(value) != Node:
             raise TypeError("next_node must be a Node object")
         else:
@@ -30,10 +54,16 @@ class Node:
 
 
 class SinglyLinkedList:
+    """ Defines a singly linked list """
     def __init__(self):
+        """ Initialize the linked list """
         self.head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts a node in the sorted position in the list
+        :param value: Integer data for the new node
+        """
         new = Node(value)
         node = self.head
         if not node or node.data > value:
@@ -46,6 +76,10 @@ class SinglyLinkedList:
             node.next_node = new
 
     def __str__(self):
+        """
+        Returns a string representation of the linked list
+        :return: String representation of the linked list
+        """
         output = ""
         node = self.head
         while node:
