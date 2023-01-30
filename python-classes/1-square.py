fb9672a0b04b9dd1dc63915600f136189b074251
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-30 11:01:51 +0100

    defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class Square that defines a square by its size attribute."""
+
+    def __init__(self, size):
+        """Instantiate a square with size."""
+        self.__size = size
