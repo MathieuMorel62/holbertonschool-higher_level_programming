7df087f40cc698ae2a8ba1f212558370d0f3226e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-30 11:45:12 +0100

    returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class Square that defines a square by its size attribute."""
+
+
+class Square:
+    """Class Square that defines a square by its size attribute."""
+
+    def __init__(self, size=0):
+        """Instantiate a square with optional size.
+
+        Args:
+        size (int): Size of the square. Defaults to 0.
+
+        Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return the current square area of a square."""
+        return self.__size ** 2
