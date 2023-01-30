d6cdc49527cf5ba49c6d32791c7a883f9d859276
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-30 15:08:53 +0100

    delete elif condition

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,11 +54,9 @@ class Square:
         Property setter to set position
         Raise TypeError if value is not a tuple of 2 positive integers
         """
-        if type(value) != tuple or len(value) != 3:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) != int or type(value[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
+        if type(value) != tuple or len(value) != 2 or \
+            type(value[0]) != int or type(value[1]) != int or \
+                value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
