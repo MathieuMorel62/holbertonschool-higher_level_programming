fd376a0b1cdd54c894748a9aa4211a577f147047
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-31 17:18:38 +0100

    add first file

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    if isinstance(a, (int, float)):
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)):
+        b = int(b)
+    else:
+        raise TypeError("b must be an integer")
+    return (a + b)
