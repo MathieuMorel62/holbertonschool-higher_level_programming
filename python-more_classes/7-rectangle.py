31f20989411e141675a3cc63acced95de2db12c5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-01 19:48:31 +0100

    pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -63,7 +63,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
         else:
-            return '\n'.join(str(self.print_symbol) * self.width for i in range(self.height))
+            return '\n'.join(str(self.print_symbol) * self.width \
+                for i in range(self.height))
 
     def __repr__(self):
         """
