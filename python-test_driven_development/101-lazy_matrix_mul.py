1909dbdfa53ee41f19921e6e1d3f5e26610d2630
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-03 00:04:42 +0100

    add matmul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,11 +2,8 @@
 """  multiplies 2 matrices by using the module NumPy """
 
 
-import numpy as np
+import numpy
 
 def lazy_matrix_mul(m_a, m_b):
     """ multiplies 2 matrices """
-    try:
-        return repr(np.matmul(m_a, m_b))
-    except ValueError as e:
-        raise ValueError("Error: Scalar operands are not allowed, use '*' instead")
+    return numpy.matmul(m_a, m_b)
