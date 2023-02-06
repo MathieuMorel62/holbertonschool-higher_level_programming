f3ed99d8f97fbbfeebd23eba0e95a177309446d6
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-06 20:04:13 +0100

    Validates value for name

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" defines a basic geometric shape. """
+
+
+class BaseGeometry:
+    """ A class that defines a basic geometric shape. """
+    def area(self):
+        """Raise an exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value for name
+        name: string, name to be used in exceptions
+        value: int, value to be validated
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
