9dd3e9f680c018411b51adfb43300b7ca3cde460
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-06 19:45:53 +0100

    pycodestyle

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,5 +5,5 @@
 class BaseGeometry:
     """ A class that defines a basic geometric shape. """
     def area(self):
-        """ Raise an exception with the message 'area() is not implemented'. """
+        """Raise an exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
