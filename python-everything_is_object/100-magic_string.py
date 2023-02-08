e9ebc1c4cda20b9bbaf29eb7cf5e4345cf59fd10
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-08 20:42:28 +0100

    return a string n times the number of the iteration

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Pythonic"""
-
-
 def magic_string(count=[]):
-    """ Returns a string “BestSchool” n times the number of the iteration """
     count.append("BestSchool")
     return ", ".join(count)
-
