da570ec145954748bb247bf217f23ce559cacc00
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-08 22:29:00 +0100

    insert a line of text to a file

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Function that inserts a line of text to a file """
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Function that inserts a line of text to a file,
+    after each line containing a specific string.
+    """
+    with open(filename, mode="r") as file:
+        content = file.readlines()
+
+    with open(filename, mode="w") as file:
+        for line in content:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
