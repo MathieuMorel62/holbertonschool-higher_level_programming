f7f11ba43ee024cc5c3182cf22532ed11f18fc8e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-08 12:49:45 +0100

    add append function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """ Returns number of lines in text file """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         return f.write(text)
