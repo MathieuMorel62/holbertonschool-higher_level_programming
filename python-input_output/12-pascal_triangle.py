3fe95504a0630ee0e573e3fe9480bb57564609e3
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-08 18:24:29 +0100

    function generates the Pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """ 
+    This function generates the Pascal triangle to line n.
+    If n is smaller or equal to 0, returns an empty list.
+
+    :Param n: The number of lines in Pascal's triangle.
+    :Return: Pascal's triangle complete to line n.
+    """
+    if n <= 0:
+        return []
+    result = [[1]]
+    for num_line in range(n-1):
+        line = [1]
+        for num_column in range(num_line):
+            new_column = (result[-1][num_column] + result[-1][num_column+1])
+            line.append(new_column)
+        line.append(1)
+        result.append(line)
+    return result
