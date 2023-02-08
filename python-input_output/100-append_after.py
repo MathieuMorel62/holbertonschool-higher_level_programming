ea6c70b112089acbb0c4693941871e5abbe2d39d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-08 22:32:15 +0100

    pycodestyle

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function that inserts a line of text to a file """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """
     Function that inserts a line of text to a file,
