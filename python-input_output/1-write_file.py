8c59707c577830566ffb9d742ecef34a68d99d75
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-08 12:30:40 +0100

    pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Write to a file """
 
+
 def write_file(filename="", text=""):
     """ Returns number of lines in text file """
     with open(filename, "w", encoding="utf-8") as f:
