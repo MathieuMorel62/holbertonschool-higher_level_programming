f9a9251275e56a9929c049efcb6f8b020aba94f5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-10 21:22:01 +0100

    script that reads stdin line by line and computes metrics

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -3,28 +3,26 @@
 import sys
 
 
-dictstatus = {}
-totalsize = 0
-totalcount = 0
-try:
-    for line in sys.stdin:
-        split = line.split()
-        status = split[-2]
-        totalsize += int(split[-1])
-        if status in dictstatus.keys():
-            dictstatus[status] += 1
-        else:
-            dictstatus[status] = 1
-        totalcount += 1
-        if totalcount == 10:
-            sortme = sorted(dictstatus.keys())
-            print("File size:", totalsize)
-            for keys in sortme:
-                print("{}: {}".format(keys, dictstatus[keys]))
-            totalcount = 0
-            continue
-except KeyboardInterrupt:
-    sortme = sorted(dictstatus.keys())
-    print("File size:", totalsize)
-    for keys in sortme:
-        print("{}: {}".format(keys, dictstatus[keys]))
+status_counts = {}
+total_size = 0
+line_count = 0
+
+for log_line in sys.stdin:
+    split_line = log_line.split()
+    status = split_line[-2]
+    total_size += int(split_line[-1])
+
+    if status in status_counts:
+        status_counts[status] += 1
+    else:
+        status_counts[status] = 1
+
+    line_count += 1
+    if line_count == 10:
+        sorted_keys = sorted(status_counts.keys())
+        print("Total size:", total_size)
+
+        for key in sorted_keys:
+            print("{}: {}".format(key, status_counts[key]))
+        line_count = 0
+        continue
