9a93ad4ce970e37a3494e01b27846f97e83ca304
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-13 16:56:50 +0100

    add parenthesis on id

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,4 +9,4 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] {self.id} {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
