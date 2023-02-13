e406a127c9af67eeac5e993ebc75cc98eea59c2f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-13 12:53:04 +0100

    adding different type errors

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,10 +17,15 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-        pass
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -28,7 +33,12 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        pass
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -36,7 +46,12 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        pass
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -44,4 +59,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        pass
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
