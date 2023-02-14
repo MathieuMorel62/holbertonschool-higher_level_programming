4b88828f1948dddd067157fb89ecc25dd3a17ecf
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-14 08:54:16 +0100

    add test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,12 +7,8 @@ class TestBase(unittest.TestCase):
    def test_instance(self):
         """ Test instantiation """
         self.assertEqual(Base().id, 1)
-        self.assertEqual(Base().id, 4)
-        self.assertEqual(Base().id, 3.5)
-        self.assertEqual(Base().id, "Holberton")
-        self.assertEqual(Base().id, ["list", 2, 5.5])
+        self.assertEqual(Base().id, 2)
         self.assertEqual(Base(89).id, 89)
-        self.assertNotEqual(Base().id, 20)
 
 if __name__ == '__main__':
     unittest.main()
