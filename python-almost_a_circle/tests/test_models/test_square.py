1c231389f443ea8e4b0d5178a8df96aab6260774
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-16 20:40:34 +0100

    adding the test

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,31 +1,11 @@
-#!/usr/bin/python3
 import unittest
-import json
-import os
-from models.base import Base
 from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.square = Square(3, 2, 3, 4)
 
-    def test_inheritance(self):
-        """ Test inheritance """
-        self.assertIsInstance(self.square, Square)
-        self.assertTrue(issubclass(type(self.square), Square))
-        self.assertNotIsInstance(self.square, list)
-        self.assertNotIsInstance(self.square, dict)
-
-    def test_size(self):
-        """ Test size property """
-        self.assertEqual(self.square.size, 3)
-        self.square.size = 10
-        self.assertEqual(self.square.size, 10)
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            self.square.size = 0
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            self.square.size = 1.5
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            self.square.size = "test"
+    def test_to_dictionary(self):
+        """Test the to_dictionary method of the Square class"""
+        s = Square(5, 2, 3, 7)
+        expected_output = {'id': 7, 'size': 5, 'x': 2, 'y': 3}
+        self.assertDictEqual(s.to_dictionary(), expected_output)
