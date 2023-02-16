3f040dffe2083f65cd7e829f69f83cb789bf4b0a
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-16 19:14:29 +0100

    test for rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,2 +1,23 @@
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
+from .test_base import TestBase
+import os
+import json
+
+
+class testRectangle(TestBase):
+
+    def test_rectangle_instance(self):
+        """Test if rectangle instance is created successfully"""
+        rect = Rectangle(1, 2)
+        self.assertIsInstance(rect, Base)
+        self.assertIsInstance(rect, Rectangle)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 0)
+        self.assertEqual(rect.y, 0)
+        self.assertEqual(rect.id, rect._Base__nb_objects)
+
+if __name__ == '__main__':
+    unittest.main()
