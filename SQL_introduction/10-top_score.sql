f7e01e78b30810c84e7c814feb27c54a27d382b1
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-13 20:08:39 +0100

    script that lists all records with a score >= 10 in the table second_table

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
 -- script that lists all records of the table (score and the name in this order)
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table 
+ORDER BY score DESC;
