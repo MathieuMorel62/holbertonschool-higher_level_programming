3ac0dded30777d4587339d5538c99ff7deba76ef
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-14 14:28:19 +0100

    creates the database hbtn_0d_2 and the user user_0d_2.

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,17 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- Creates the database
+CREATE DATABASE
+    IF NOT EXISTS user_0d_2;
+
+-- Create the user with the password
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Gives the user only the SELECT privilege.
+GRANT SELECT
+    ON hbtn_0d_2.*
+    TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
