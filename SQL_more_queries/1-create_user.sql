805b35abb888da479b8c028a54bd6675896f43eb
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-14 13:58:37 +0100

    script that creates the MySQL server user user_0d_1.

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,13 @@
+-- script that creates the MySQL server user user_0d_1.
+-- Creates a new user with the name user_0d_1 and a password user_0d_1_pwd
+CREATE USER 
+IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Gives the user all privileges on all databases ('.') using the host 'localhost
+GRANT ALL PRIVILEGES 
+ON *.* 
+TO 'user_0d_1'@'localhost';
+
+-- Updates user privileges to take into account the changes we have just made.
+FLUSH PRIVILEGES;
