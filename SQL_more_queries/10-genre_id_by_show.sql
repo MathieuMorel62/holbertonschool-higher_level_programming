e6f69935cc7e16dbc1599f2bdeb945e2ed01f0e1
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-14 20:22:18 +0100

    list shows and genres : lists all shows, and all genres linked to that show, from the database

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,5 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title, genre_id ASC;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,5 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states
-ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
