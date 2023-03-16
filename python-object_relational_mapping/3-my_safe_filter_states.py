724db523881e1af78c9b3f0bbaf0c201a81ddad6
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-16 12:36:59 -0700

    This script takes in arguments and displays all values in the states table     of hbtn_0e_0_usa where name matches the argument.     But this time, it is safe from MySQL injections!

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-    Displays all values in the states table of hbtn_0e_0_usa
-    where name matches the argument
+    This script takes in arguments and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument.
+    But this time, it is safe from MySQL injections!
 """
 
 
@@ -24,7 +25,8 @@ if __name__ == "__main__":
 
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state_name)
+        "SELECT * FROM states WHERE BINARY name = %s ORDER BY id",
+        (state_name,)
         )
 
     results = cursor.fetchall()
