f6beb317d8a6d0e86dc52b7a8f22a9af5e0ec77a
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-16 11:36:55 -0700

    Displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+        )
+
+    cursor = connection.cursor()
+    cursor.execute(
+        f"SELECT * FROM states WHERE name LIKE BINARY '{sys.argv[4]}'")
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    connection.close()
