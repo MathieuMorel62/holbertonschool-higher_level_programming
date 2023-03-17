52c4fc23acf337f64db1a77ecb6659bc8c9ffbee
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-17 03:16:25 -0700

    Prints the State object with the name passed as argument     from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    Prints the State object with the name passed as argument
+    from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Create engine to connect to database
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create session to interact with database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query for state with matching name
+    query = session.query(State).filter(State.name == state_name)
+
+    # Print state ID if found, else print "Not found"
+    state = query.first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
