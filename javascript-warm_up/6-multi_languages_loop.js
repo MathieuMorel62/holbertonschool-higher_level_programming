ef19c8b0ce216c0a1e00465a380c45419368ff12
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-12 15:15:15 +0200

    Script that prints 3 lines with a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Script that prints 3 lines with a loop */
+
+const words = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+words.forEach(function (words) {
+  console.log(words);
+});
