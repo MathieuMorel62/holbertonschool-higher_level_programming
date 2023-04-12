6a07a1f4ac978204c9e2f3bc9673e41a3827039e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-12 13:37:30 +0200

    Implementation of the verification of the number of arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Management of command line arguments */
+
+const numberOfArguments = process.argv.length - 2;
+
+if (numberOfArguments === 0) {
+    console.log('No argument');
+} else if (numberOfArguments === 1) {
+    console.log('Argument found');
+} else {
+    console.log("Arguments found");
+}
