c91d61c878c34bb71ba55ac7eb2f131b5a47894d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-12 23:21:54 +0200

    add print method

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/* Add constructor validation to Rectangle class */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
