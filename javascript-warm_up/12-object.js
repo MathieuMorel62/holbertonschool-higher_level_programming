13eb5c17eb6d74e10bb758f6d158a45d905baaf1
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-12 20:20:00 +0200

    Script to replace the value

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Script to replace the value */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject);
