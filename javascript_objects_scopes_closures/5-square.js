86b75000586948a55f5b1d560cc5a44a9b171851
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-13 18:24:37 +0200

    Add implementation for Square class that inherits from Rectangle class

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Add Square class that inherits from Rectangle */
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
