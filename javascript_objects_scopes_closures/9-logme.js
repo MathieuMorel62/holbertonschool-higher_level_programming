35f032e130f67193c31d825521483466c9117bb5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-13 19:40:42 +0200

    Function that prints the number of arguments already printed

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Function that prints the number of arguments already printed */
+
+let countArgs = 0;
+exports.logMe = function (item) {
+  console.log(countArgs, ':', item);
+  countArgs += 1;
+};
