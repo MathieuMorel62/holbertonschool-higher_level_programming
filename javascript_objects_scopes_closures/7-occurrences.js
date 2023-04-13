32090cdbdff78cee48714e28b14b15543bdc52c2
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-13 18:53:56 +0200

    Implement nbOccurences function to return the number of occurrences of an element in a list.

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Add implementation of nbOccurences function */
+
+exports.nbOccurences = function (list, searchElement) {
+  let countOccurences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      countOccurences += 1;
+    }
+  }
+  return countOccurences;
+}
