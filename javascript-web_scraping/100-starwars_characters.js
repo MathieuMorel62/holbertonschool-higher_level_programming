f87c0e570447fec71ae86ac974249103159013db
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-17 22:32:06 +0200

    Script that prints all characters of a Star Wars movie

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* Script that prints all characters of a Star Wars movie */
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const filmApiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(filmApiUrl, function(filmError, filmResponse, filmBody) {
+  if (filmError) {
+    console.log(filmError);
+  } else {
+    const filmData = JSON.parse(filmBody);
+
+    for (const characterApiUrl of filmData.characters) {
+      request(characterApiUrl, function(characterError, characterResponse, characterBody) {
+        if (characterError) {
+          console.error(characterError);
+        } else {
+          const characterData = JSON.parse(characterBody);
+          console.log(characterData.name);
+        }
+      });
+    }
+  }
+});
