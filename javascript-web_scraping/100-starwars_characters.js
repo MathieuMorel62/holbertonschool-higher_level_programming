475b8f937c8f86c90edfbe7ccf6c5e8e93d9dd1e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-17 22:33:25 +0200

    semistandard validation

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -6,14 +6,14 @@ const request = require('request');
 const movieId = process.argv[2];
 const filmApiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(filmApiUrl, function(filmError, filmResponse, filmBody) {
+request(filmApiUrl, function (filmError, filmResponse, filmBody) {
   if (filmError) {
     console.log(filmError);
   } else {
     const filmData = JSON.parse(filmBody);
 
     for (const characterApiUrl of filmData.characters) {
-      request(characterApiUrl, function(characterError, characterResponse, characterBody) {
+      request(characterApiUrl, function (characterError, characterResponse, characterBody) {
         if (characterError) {
           console.error(characterError);
         } else {
