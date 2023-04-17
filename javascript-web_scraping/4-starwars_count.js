69730f8c70081432d419fbf38098d289224e9c37
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-17 18:07:11 +0200

    Add a loop

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,20 +5,19 @@ const request = require('request');
 
 const starWarsFilmsApiUrl = process.argv[2];
 
-request.get(starWarsFilmsApiUrl, function (error, response, body) {
+request(starWarsFilmsApiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+      const characters = film.characters;
+      if (characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    });
+    console.log(count);
   }
-
-  const films = JSON.parse(body).results;
-
-  const wedgeAntillesFilms = films.filter(function (film) {
-    const charactersUrl = film.characters;
-    const wedgeAntillesUrl = 'https://swapi-api.hbtn.io/api/people/18/';
-    const wedgeAntillesIsPresent = charactersUrl.includes(wedgeAntillesUrl);
-    return wedgeAntillesIsPresent;
-  });
-
-  const numberOfWedgeAntillesFilms = wedgeAntillesFilms.length;
-  console.log(numberOfWedgeAntillesFilms);
 });
